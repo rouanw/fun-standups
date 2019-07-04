b5c521ac4922939f2434659a57158648481c9658
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2019-07-04 11:49:44 +0200

    Make grid

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,7 @@ const Header = ({ siteTitle }) => (
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
+        maxWidth: 1600,
         padding: `1.45rem 1.0875rem`,
       }}
     >
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,44 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import "../components/format.css"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="container">
+      <div className="formats">
+        <div className="format">
+          <div className="format-title">The title</div>
+          <img className="format-image" src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 1x, https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
+          <div className="format-description">This is a description This is a descriptionThis is a description This is a description</div>
+        </div>
+        <div className="format">
+          <div className="format-title">The title</div>
+          <img className="format-image" src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 1x, https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
+          <div className="format-description">This is a description This is a descriptionThis is a description This is a description</div>
+        </div>
+        <div className="format">
+          <div className="format-title">The title</div>
+          <img className="format-image" src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 1x, https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
+          <div className="format-description">This is a description This is a descriptionThis is a description This is a description</div>
+        </div>
+        <div className="format">
+          <div className="format-title">The title</div>
+          <img className="format-image" src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 1x, https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
+          <div className="format-description">This is a description This is a descriptionThis is a description This is a description</div>
+        </div>
+        <div className="format">
+          <div className="format-title">The title</div>
+          <img className="format-image" src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 1x, https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
+          <div className="format-description">This is a description This is a descriptionThis is a description This is a description</div>
+        </div>
+        <div className="format">
+          <div className="format-title">The title</div>
+          <img className="format-image" src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 1x, https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
+          <div className="format-description">This is a description This is a descriptionThis is a description This is a description</div>
+        </div>
+      </div>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
