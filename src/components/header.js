90d330b1f762c732f4ec13be3e35ca01037e686c
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2019-07-04 10:29:49 +0200

    change background and site title

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#43adacc4`,
       marginBottom: `1.45rem`,
     }}
   >
