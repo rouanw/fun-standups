dddb9c271608bae0f2bd808c50167c9fda216c12
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2019-07-05 16:41:03 +0200

    style format details page

--- src/components/format-details.js
+++ src/components/format-details.js
@@ -2,19 +2,32 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import "../components/format.css"
-import Format from "../components/format"
+import "../components/format-details.css"
 
-const FormatDetails = ({ pageContext } ) => {
-  const { format } = pageContext;
+const FormatDetails = ({ pageContext }) => {
+  const { format } = pageContext
   return (
     <Layout>
       <SEO title="TBC" />
-      <div className="container">
-        <Format title={format.title} summary={format.summary}></Format>
+
+      <div className="content">
+        <div className="white-board">
+        <h2>{format.title}</h2>
+        <h3>Summary</h3>
+        <p>{format.summary}</p>
+        <h3>Description</h3>
+        <p>{format.description}</p>
+        <h3>Benefits</h3>
+        <ul>
+          {format.benefits.map((benefit)=><li>{benefit}</li>)}
+          </ul>
+          <div className="eraser"></div>
+          <div className="red-pen"></div>
+          <div className="blue-pen"></div>
+        </div>
       </div>
     </Layout>
-  );
+  )
 }
 
 export default FormatDetails
--- src/format-data.js
+++ src/format-data.js
@@ -2,13 +2,16 @@ module.exports = [{
   slug: 'update-the-next-person',
   title:"Give an update to the next person",
   summary:"Each person gives an update to the next person in the team, using their name and making eye contact",
-
 },
 {
   slug: "one-thing-only",
   title: "One thing only",
   summary:"Each team member only talks about the single most important thing they want to tell people",
-
+  description:"Take turns giving updates. Each team member only talks about the single most important thing they want to tell people.",
+  benefits:[
+    "Helps keep standups short and focused",
+    "Helps encourage team members to use other communication channels to talk about things that aren’t crucial for standup - i.e. it encourages people to actually talk to each other outside of standup"
+  ]
 },
 {
   slug: "zoom-way-out",
