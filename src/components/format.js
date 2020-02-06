90d9efdfa2eadbb35807445e7dab143cce5e12a2
rouanw <Rouan.Wilsenach@tesglobal.com>
2020-02-06 10:46:24 +0200

    grow formats on hover

--- src/components/format.js
+++ src/components/format.js
@@ -3,11 +3,13 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Format = ({ title, summary, slug }) => (
-  <div className="format">
+  <div className="format-wrapper">
     <Link to={`/${slug}`}>
-      <div className="format-title">{title}</div>
+      <div className="format">
+        <div className="format-title">{title}</div>
+        <div className="format-summary">{summary}</div>
+      </div>
     </Link>
-    <div className="format-summary">{summary}</div>
   </div>
 )
 
