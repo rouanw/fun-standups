369070b96259e83024c39fcd53cc767178ab82f9
aepicos <github@aepicos.com>
2021-06-10 17:54:36 +0100

    html clean-up with semantic tags and hierarchy

--- src/components/RelatedStandups.js
+++ src/components/RelatedStandups.js
@@ -9,8 +9,10 @@ const RelatedStandups = ({ tags, title }) => {
   const relatedStandups = allStandups.filter((standup) => standup.title !== title && _.intersection(standup.tags, tags).length).slice(0, 3);
   return Boolean(relatedStandups.length) && (
     <div className="related-standups">
-      <div className="related-standups-header">Try these next</div>
-     {relatedStandups.map((standup) => <Standup key={standup.title} standup={standup} tagColors={getTagColors()} />)}
+      <h2 className="related-standups--header">Try these next</h2>
+      <ul>
+        {relatedStandups.map((standup) => <Standup key={standup.title} standup={standup} tagColors={getTagColors()} />)}
+      </ul>
     </div>
   )
 }
