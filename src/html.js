de7f47a76665168a590aabbbf4849530940d5dd6
aepicos <github@aepicos.com>
2021-06-10 11:31:44 +0100

    added ubuntu font for headings

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <link rel="preload" href="/fonts/ubuntu.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
