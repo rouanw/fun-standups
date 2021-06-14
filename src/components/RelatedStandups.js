4681b457b27bd8f1b5af8aedea8beee545fb63e8
aepicos <github@aepicos.com>
2021-06-14 12:02:04 +0100

    changed .related-standups to a more semantic aside

--- src/components/RelatedStandups.js
+++ src/components/RelatedStandups.js
@@ -9,12 +9,12 @@ const RelatedStandups = ({ tags, title }) => {
   const shuffledStandups = allStandups.sort(() => Math.random() - 0.5)
   const relatedStandups = shuffledStandups.filter((standup) => standup.title !== title && _.intersection(standup.tags, tags).length).slice(0, 3);
   return Boolean(relatedStandups.length) && (
-    <div className="related-standups">
+    <aside className="related-standups">
       <h2 className="related-standups--header">Try these next</h2>
       <ul className="standups">
         {relatedStandups.map((standup) => <Standup key={standup.title} standup={standup} tagColors={getTagColors()} />)}
       </ul>
-    </div>
+    </aside>
   )
 }
 
