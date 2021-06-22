a8eb4537382e7996ad1a1abf25ed66eaa4e65c0c
rouanw <rouanw@users.noreply.github.com>
2021-06-22 15:01:13 +0200

    stick to a simpler related standup list for now

--- src/components/RelatedStandups.js
+++ src/components/RelatedStandups.js
@@ -6,8 +6,7 @@ import Standup from "./standup";
 import getTagColors from "../get-tag-colors";
 
 const RelatedStandups = ({ tags, title }) => {
-  const shuffledStandups = allStandups.sort(() => Math.random() - 0.5)
-  const relatedStandups = shuffledStandups.filter((standup) => standup.title !== title && _.intersection(standup.tags, tags).length).slice(0, 3);
+  const relatedStandups = allStandups.filter((standup) => standup.title !== title && _.intersection(standup.tags, tags).length).slice(0, 3);
   return Boolean(relatedStandups.length) && (
     <aside className="related-standups">
       <h2 className="related-standups--header">Try these next</h2>
