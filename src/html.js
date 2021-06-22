ee96741fca54afef7ffd165de49cf8414857cd2b
rouanw <rouanw@users.noreply.github.com>
2021-06-22 15:03:03 +0200

    update case

--- src/html.js
+++ src/html.js
@@ -11,7 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link rel="preload" href="/fonts/montserrat.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
+        <link rel="preload" href="/fonts/montserrat.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
